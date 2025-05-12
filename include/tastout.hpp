9187f4abc61b9de38801309bcb812fbc8104b29e
Gabriel ROBERTO LIMA <gabriel.roberto-lima@ganil.fr>
2025-05-12 13:49:04 +0200

    spelling corretion

--- include/tastout.hpp
+++ include/tastout.hpp
@@ -4,9 +4,9 @@
 #include <string>
 #include <algorithm>
 
-//Should we work with different types to tatoo or we work with a string to be tatooed?
+//Should we work with different types to tattoo or we work with a string to be tattooed?
 //For the last bit change option, should we receive a string or a vector to work with?
-//	  Will we send all the bytes and a header or the header will also be included as part of tatoo? 
+//	  Will we send all the bytes and a header or the header will also be included as part of tattoo? 
 class Tastout
 {
 
@@ -14,29 +14,29 @@ public:
 	
 	//! Constructor
 	/**
-	 * \param [in] magicNumber magic number to include in tatooed signal
-	 * \param [in] isTatooInHead define if tatoo is in head(true) or in tail(false)
+	 * \param [in] magicNumber magic number to include in tattooed signal
+	 * \param [in] isTattooInHead define if tattoo is in head(true) or in tail(false)
 	 **/
-	 Tastout(const std::string & magicNumber, const bool & isTatooInHead = false) : magicNumber_(magicNumber), isTatooInHead_(isTatooInHead){}
+	 Tastout(const std::string & magicNumber, const bool & isTattooInHead = false) : magicNumber_(magicNumber), isTattooInHead_(isTattooInHead){}
 	 
-	 //! Write function tatoos the signal 
+	 //! Write function tattoos the signal 
 	 /**
 	  * \param [in] signal input signal
-	  * \param [in] value value to be tatooed
-	  * \param [out] tatooedSignal tatooed signal
+	  * \param [in] value value to be tattooed
+	  * \param [out] tattooedSignal tattooed signal
 	  **/
-	 char write(const std::string & signal, const std::string & value, std::string & tatooedSignal)
+	 char write(const std::string & signal, const std::string & value, std::string & tattooedSignal)
 	 {
-		//! Verify if the signal can be tatooed
+		//! Verify if the signal can be tattooed
 		if(signal.size() <=  value.size() + magicNumber_.size()) return 1;
 		
-		if(isTatooInHead_)
+		if(isTattooInHead_)
 		{
 			//! Reverse signal to take tail
 			std::string reverseSignal(signal);
 			reverse(reverseSignal.begin(),reverseSignal.end());
-			//! Tatoos the signal concatenating the magic number, the value and the most significatives digits of signal
-			tatooedSignal = magicNumber_ + value + reverseSignal.substr(0, signal.size() - (value.size() + magicNumber_.size()));			
+			//! Tattoos the signal concatenating the magic number, the value and the most significatives digits of signal
+			tattooedSignal = magicNumber_ + value + reverseSignal.substr(0, signal.size() - (value.size() + magicNumber_.size()));			
 		}else
 		{
 			//! Reverse magic number
@@ -47,22 +47,28 @@ public:
 			std::string reverseValue(value);
 			reverse(reverseValue.begin(),reverseValue.end());
 			
-			//! Tatoos the signal concatenating the most significatives digits of signal, the reversed value and and the reversed magic number
-			tatooedSignal = signal.substr(0, signal.size() - (value.size() + magicNumber_.size())) + reverseValue + reverseMagicNumber;
-		}//isTatooInHead
+			//! Tattoos the signal concatenating the most significatives digits of signal, the reversed value and and the reversed magic number
+			tattooedSignal = signal.substr(0, signal.size() - (value.size() + magicNumber_.size())) + reverseValue + reverseMagicNumber;
+		}//isTattooInHead
 		return 0;
 	 }
 	 
-	 char read()
+	 char read(const std::string & tattooedSignal, std::string & untattooedSignal, std::string & value)
 	 {
-		
+		std::string reverseMagicNumber(magicNumber_);
+		reverse(reverseMagicNumber.begin(), reverseMagicNumber.end());
+	
+		if(tattooedSignal.find(magicNumber_) == 0)
+		{
+			std::cout << "Type: " << tattooedSignal.substr(7,2) << std::endl;
+		}
 		
 		return 0;
 	 }	 
  private:
  
 	const std::string magicNumber_;
-	const bool isTatooInHead_;
+	const bool isTattooInHead_;
 };
 
 
