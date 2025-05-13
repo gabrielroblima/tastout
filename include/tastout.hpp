00b88784873b8c86e088f7017de77acb328df8da
Gabriel ROBERTO LIMA <gabriel.roberto-lima@ganil.fr>
2025-05-13 10:45:46 +0200

    Read tattooed values function

--- include/tastout.hpp
+++ include/tastout.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 #include <iomanip>
 #include <sstream>
 #include <cstdlib>
@@ -27,7 +28,7 @@ public:
 	 //! Write function tattoos the signal output type char should be considered as int8
 	 /**
 	  * \param [in] signal input signal
-	  * \param [in] value values to be tattooed
+	  * \param [in] value vector of values to be tattooed
 	  * \param [out] tattooedSignal tattooed signal
 	  **/
 	 char write(const std::string & signal, const std::vector<unsigned char> & value, std::string & tattooedSignal)
@@ -41,13 +42,13 @@ public:
 		//! T08 + b|B + (0~F) + i		
 		std::ostringstream tastoutStream;
 		
-		//! Tattoos the signal concatenating the magic number and values
+		//! Tattoo the signal concatenating the magic number and values
 		tastoutStream << magicNumber_ << "T08b" << std::hex << value.size() << "i";
 		for(int i = 0; i < value.size(); i++){tastoutStream << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(value[i]);}		
 
 		if(isTattooInHead_)
 		{			
-			//! Concatenate the first digits of signal with tastout
+			//! Concatenate the first digits of signal with tattoo
 			tattooedSignal = tastoutStream.str() + signal.substr(0, signal.size() - tastoutStream.str().size());
 		}else
 		{
@@ -55,26 +56,56 @@ public:
 			std::string tastoutString(tastoutStream.str());
 			reverse(tastoutString.begin(), tastoutString.end());
 			
+			//! Concatenate reversed tattoo with the first digits of signal 
 			tattooedSignal = signal.substr(0, signal.size() - tastoutStream.str().size()) + tastoutString;
 		}//isTattooInHead
 		return 0;
 	 }
 	 
-	 char read(const std::string & tattooedSignal, std::vector<unsigned char> & value)
+	 //! Read function get data tattooed into string type char should be considered as int8
+	 /**
+	  * \param [in] tattooedSignal input signal
+	  * \param [in] values vector of values tattooed
+	  **/
+	 char read(std::string & tattooedSignal, std::vector<unsigned char> & values)
 	 {
-		//~ std::string reverseMagicNumber(magicNumber_);
-		//~ reverse(reverseMagicNumber.begin(), reverseMagicNumber.end());
+		std::string reverseMagicNumber(magicNumber_);
+		reverse(reverseMagicNumber.begin(), reverseMagicNumber.end());
 	
-		//~ if(tattooedSignal.find(magicNumber_) == 0)
-		//~ {
-			//~ value = tattooedSignal.substr(12, 8);
-		//~ }else if(tattooedSignal.find(reverseMagicNumber))
-		//~ {
-			//~ value = tattooedSignal;
-			//~ reverse(value.begin(), value.end());
-			//~ value = tattooedSignal.substr(11, 8);
-			//~ reverse(value.begin(), value.end());
-		//~ }
+		//! Verify if the magic number is in head or tail 
+		bool wasTattooFound = false;
+		if(tattooedSignal.find(magicNumber_) == 0)
+		{
+			wasTattooFound = true;			
+		}else if(tattooedSignal.find(reverseMagicNumber) != tattooedSignal.npos)
+		{
+			wasTattooFound = true;
+			//! Reverse tattoo if the magic number is reversed
+			reverse(tattooedSignal.begin(), tattooedSignal.end());
+		}
+		
+		if(not wasTattooFound) return 1;
+		
+		//! Verify size of values and allocates memory if necessary
+		size_t neededSizeOfValues = std::stoi(tattooedSignal.substr(11,1), nullptr, 16);
+		if(values.size() < neededSizeOfValues)
+		{
+			values.reserve(neededSizeOfValues);
+			values.resize(neededSizeOfValues);
+		}
+		
+		unsigned int byte;
+		std::stringstream stringStream;
+		for(size_t i = 0; i < neededSizeOfValues; i++)
+		{
+			//! Copy byte to stream. The offset "13" represents the number of chars before the first value
+			stringStream << std::hex << tattooedSignal.substr(2*i + 13, 2);
+			stringStream >> byte;
+			//! Copy byte to vector at position i
+			memcpy(&values[i], &byte, 1);
+			stringStream.clear();
+		}
+		
 		return 0;
 	 }	 
  private:
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main (int argc, char** argv)
 	Tastout tastout("TAsTout", false);
 	
 	std::string signal("1632641282565121024204840968192");
-	std::vector<unsigned char> input = {0x08, 0xAB, 0xCD, 0xEF};
+	std::vector<unsigned char> input = {0x08, 0xAB, 0xCD, 0xEF, 0xB1};
 	std::string tattooedSignal;
 	
 	if(tastout.write(signal, input, tattooedSignal) != 0) return EXIT_FAILURE;
@@ -18,6 +18,12 @@ int main (int argc, char** argv)
 	
 	std::vector<unsigned char> output;
 	tastout.read(tattooedSignal, output);
+	
+	std::cout << "Read values:" << std::endl;
+	for(size_t i =0; i < output.size(); i++)
+	{
+		std::cout << i << ": " << std::hex << std::setfill('0') << std::setw(2) << static_cast<unsigned int>(output[i]) << std::endl;
+	}
 	
 	return EXIT_SUCCESS;
 }
